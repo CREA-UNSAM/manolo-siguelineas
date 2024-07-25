9d1bf70c209cbe2bde241605f6e7d67903670e5b
sebav <sebavacc6365@gmail.com>
2024-07-25 10:29:19 -0300

    add ESP82Web

---
+++ ESP8266-WebServer-example/ESP8266-WebServer-example.ino
@@ -0,0 +1,82 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "ManoloWiFi";
+const char* password = "Manolo2345";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+char rcvd;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+
+        if(Serial.available() > 0) {
+          rcvd = Serial.read();
+          if (rcvd == '\n' || rcvd == '\r' || rcvd == '\0') {
+            //Serial.println();
+            client.println();
+          }
+          else{
+            client.print(rcvd);
+            //Serial.print(rcvd);
+          }
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
